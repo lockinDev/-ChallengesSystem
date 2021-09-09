d84caa14cc2a690ddfd48a9bac9625da098e6713
Javier <javieralmario12@gmail.com>
2021-09-08 21:10:04 -0400

    Added Challenge class, represent to solve a multiplication

---
+++ src/main/java/com/devlockin/multiplication/domain/challenge/Challenge.java
@@ -0,0 +1,14 @@
+package com.devlockin.multiplication.domain.challenge;
+
+import lombok.*;
+/**
+* This class represents a Challenge to solve a Multiplication (a * b).
+*/
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class Challenge {
+    private int factorA;
+    private int factorB;
+}
--- src/main/java/com/devlockin/multiplication/domain/user/User.java
+++ src/main/java/com/devlockin/multiplication/domain/user/User.java
@@ -1,4 +1,4 @@
-package com.devlockin.multiplication.domain;
+package com.devlockin.multiplication.domain.user;
 
 import lombok.*;
 /**
