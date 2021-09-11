ac081d11e63f6cdd8461fb969d5a0b1f58ea168b
Javier <javieralmario12@gmail.com>
2021-09-10 21:16:15 -0400

    Added ChallengeAttemp class, validate if one challenge is correct or not

---
+++ src/main/java/com/devlockin/multiplication/domain/challenge/ChallengeAttempt.java
@@ -0,0 +1,18 @@
+package com.devlockin.multiplication.domain.challenge;
+
+import lombok.*;
+/**
+* Identifies the attempt from a {@link User} to solve a challenge.
+*/
+@Getter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+public class ChallengeAttempt {
+    private Long id;
+    private Long userId;
+    private int factorA;
+    private int factorB;
+    private int resultAttempt;
+    private boolean correct;
+}
