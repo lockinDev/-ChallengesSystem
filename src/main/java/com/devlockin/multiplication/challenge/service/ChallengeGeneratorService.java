61613e743ef03443efc9d822e256a0c0e0896f9f
Javier <javieralmario12@gmail.com>
2021-09-13 21:21:47 -0400

    Added interface to generate random challenges

---
+++ src/main/java/com/devlockin/multiplication/challenge/service/ChallengeGeneratorService.java
@@ -0,0 +1,10 @@
+package com.devlockin.multiplication.challenge.service;
+
+import com.devlockin.multiplication.challenge.domain.Challenge;
+
+public interface ChallengeGeneratorService {
+	/**
+	 * @return a randomly-generated challenge with factors between 11 and 99
+	 */
+	Challenge randomChallenge();
+}
