b3337ed9f5725adba74a6140f5bdd0dccab739d7
Javier <javieralmario12@gmail.com>
2021-09-19 22:08:38 -0400

    Added test version of ChallengeGeneratorServiceImpl, use Mockito to replace the behavior using the BDD aproach, with JUnit5

---
+++ src/test/java/com/devlockin/test/multiplication/challenge/service/ChallengeGeneratorServiceTest.java
@@ -0,0 +1,15 @@
+package com.devlockin.test.multiplication.challenge.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import java.util.Random;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.BDDMockito.given;
+
+
+public class ChallengeGeneratorServiceTest {
+
+}
