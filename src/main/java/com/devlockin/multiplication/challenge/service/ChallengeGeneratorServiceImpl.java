3b75fbc36a5a78a784e9701c7c61fdcbc595e8f6
Javier <javieralmario12@gmail.com>
2021-09-23 22:18:35 -0400

    Now added the funcionality to the test pass using BDD.

--- src/main/java/com/devlockin/multiplication/challenge/service/ChallengeGeneratorServiceImpl.java
+++ src/main/java/com/devlockin/multiplication/challenge/service/ChallengeGeneratorServiceImpl.java
@@ -8,18 +8,25 @@ import java.util.Random;
 
 @Service
 public class ChallengeGeneratorServiceImpl implements ChallengeGeneratorService {
-	private final Random random;
+    private final static int MINIMUM_FACTOR = 11;
+    private final static int MAXIMUM_FACTOR = 100;
 
-	ChallengeGeneratorServiceImpl() {
-		this.random = new Random();
-	}
+    private final Random random;
 
-	public ChallengeGeneratorServiceImpl(final Random random) {
-		this.random = random;
-	}
+    ChallengeGeneratorServiceImpl() {
+        this.random = new Random();
+    }
 
-	@Override
-	public Challenge randomChallenge() {
-		return null;
-	}
+    protected ChallengeGeneratorServiceImpl(final Random random) {
+        this.random = random;
+    }
+
+    private int next() {
+        return random.nextInt(MAXIMUM_FACTOR - MINIMUM_FACTOR) + MINIMUM_FACTOR;
+    }
+
+    @Override
+    public Challenge randomChallenge() {
+        return new Challenge(next(), next());
+    }
 }
--- src/test/java/com/devlockin/test/multiplication/challenge/service/ChallengeGeneratorServiceTest.java
+++ src/test/java/com/devlockin/test/multiplication/challenge/service/ChallengeGeneratorServiceTest.java
@@ -5,11 +5,33 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.devlockin.multiplication.challenge.domain.Challenge;
+import com.devlockin.multiplication.challenge.service.ChallengeGeneratorService;
+import com.devlockin.multiplication.challenge.service.ChallengeGeneratorServiceImpl;
+
 import java.util.Random;
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.mockito.BDDMockito.given;
 
-
+@ExtendWith(MockitoExtension.class)
 public class ChallengeGeneratorServiceTest {
+	private ChallengeGeneratorService challengeGeneratorService;
+	@Spy
+	private Random random;
 
+	@BeforeEach
+	public void setUp() {
+		challengeGeneratorService = new ChallengeGeneratorServiceImpl(random);
+	}
+
+	@Test
+	public void generateRandomFactorIsBetweenExpectedLimits() {
+		// 89 is max - min range
+		given(random.nextInt(89)).willReturn(20, 30);
+		// when we generate a challenge
+		Challenge challenge = challengeGeneratorService.randomChallenge();
+		// then the challenge contains factors as expected
+		then(challenge).isEqualTo(new Challenge(31, 41));
+	}
 }
