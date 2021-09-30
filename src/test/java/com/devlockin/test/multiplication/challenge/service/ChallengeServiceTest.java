0128b2f518d68c6bee13749a24a202319391037e
Javier <javieralmario12@gmail.com>
2021-09-29 23:34:34 -0400

    Added test class, ChallengeServiceTest that fail of course

---
+++ src/test/java/com/devlockin/test/multiplication/challenge/service/ChallengeServiceTest.java
@@ -0,0 +1,40 @@
+package com.devlockin.test.multiplication.challenge.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.devlockin.multiplication.challenge.domain.ChallengeAttempt;
+import com.devlockin.multiplication.challenge.helper.ChallengeAttemptDTO;
+import com.devlockin.multiplication.challenge.service.ChallengeService;
+import com.devlockin.multiplication.challenge.service.ChallengeServiceImpl;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class ChallengeServiceTest {
+	private ChallengeService challengeService;
+
+	@BeforeEach
+	public void setUp() {
+		challengeService = new ChallengeServiceImpl();
+	}
+
+	@Test
+	public void checkCorrectAttemptTest() {
+		// given
+		ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(50, 60, "test", 3000);
+		// when
+		ChallengeAttempt resultAttempt = challengeService.verifyAttempt(attemptDTO);
+		// then
+		then(resultAttempt.isCorrect()).isTrue();
+	}
+
+	@Test
+	public void checkWrongAttemptTest() {
+		// given
+		ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(50, 60, "test", 5000);
+		// when
+		ChallengeAttempt resultAttempt = challengeService.verifyAttempt(attemptDTO);
+		// then
+		then(resultAttempt.isCorrect()).isFalse();
+	}
+}
