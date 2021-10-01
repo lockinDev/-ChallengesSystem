de1b5d3e9224009b21a6fd31efde9b0ff8561c3a
Javier <javieralmario12@gmail.com>
2021-09-30 23:39:36 -0400

    Modified the ChallengeServiceImpl class, wrote the verifyAttemp method

--- src/main/java/com/devlockin/multiplication/challenge/service/ChallengeServiceImpl.java
+++ src/main/java/com/devlockin/multiplication/challenge/service/ChallengeServiceImpl.java
@@ -4,11 +4,20 @@ import org.springframework.stereotype.Service;
 
 import com.devlockin.multiplication.challenge.domain.ChallengeAttempt;
 import com.devlockin.multiplication.challenge.helper.ChallengeAttemptDTO;
+import com.devlockin.multiplication.user.domain.User;
 
 @Service
 public class ChallengeServiceImpl implements ChallengeService {
 	@Override
 	public ChallengeAttempt verifyAttempt(ChallengeAttemptDTO attemptDTO) {
-		return null;
+		boolean isCorrect = attemptDTO.getGuess() == attemptDTO.getFactorA() * attemptDTO.getFactorB();
+		// We don't use identifiers for now
+		User user = new User(null, attemptDTO.getUserAlias());
+
+		// Builds the domain object. Null id for now.
+		ChallengeAttempt checkedAttempt = new ChallengeAttempt(null, null, attemptDTO.getFactorA(),
+				attemptDTO.getFactorB(), attemptDTO.getGuess(), isCorrect);
+		return checkedAttempt;
+
 	}
 }
