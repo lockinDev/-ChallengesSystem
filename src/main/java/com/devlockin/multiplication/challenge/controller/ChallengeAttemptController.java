f9076cbb90ec23a3c1f28e6483a07bb6dffcae96
Javier <javieralmario12@gmail.com>
2021-10-10 22:34:42 -0400

    go back to the ChallengeAttemptController and implement the method.

--- src/main/java/com/devlockin/multiplication/challenge/controller/ChallengeAttemptController.java
+++ src/main/java/com/devlockin/multiplication/challenge/controller/ChallengeAttemptController.java
@@ -2,19 +2,31 @@ package com.devlockin.multiplication.challenge.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devlockin.multiplication.challenge.domain.ChallengeAttempt;
+import com.devlockin.multiplication.challenge.helper.ChallengeAttemptDTO;
 import com.devlockin.multiplication.challenge.service.ChallengeService;
+
 /**
-* This class provides a REST API to POST the attempts from users.
-*/
+ * This class provides a REST API to POST the attempts from users.
+ */
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/attempts")
 public class ChallengeAttemptController {
-	
+
 	private final ChallengeService challengeService;
+
+	@PostMapping
+	ResponseEntity<ChallengeAttempt> postResult(@RequestBody ChallengeAttemptDTO challengeAttemptDTO) {
+		return ResponseEntity.ok(challengeService.verifyAttempt(challengeAttemptDTO));
+	}
 
 }
