b0a2b48fcbc2889766a26a6071191a05cdc0441c
Javier <javieralmario12@gmail.com>
2021-10-21 22:49:10 -0400

    Modified user and challengeattempt clases, added jpa annotations and repository capabilities Modified the challengeattempt test class, the class constructor.

--- src/main/java/com/devlockin/multiplication/challenge/domain/ChallengeAttempt.java
+++ src/main/java/com/devlockin/multiplication/challenge/domain/ChallengeAttempt.java
@@ -1,16 +1,24 @@
 package com.devlockin.multiplication.challenge.domain;
 
 import lombok.*;
+import javax.persistence.*;
+
+import com.devlockin.multiplication.user.domain.User;
+
 /**
 * Identifies the attempt from a {@link User} to solve a challenge.
 */
-@Getter
-@ToString
-@EqualsAndHashCode
+@Entity
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ChallengeAttempt {
+    @Id
+    @GeneratedValue
     private Long id;
-    private Long userId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
     private int factorA;
     private int factorB;
     private int resultAttempt;
--- src/main/java/com/devlockin/multiplication/user/domain/User.java
+++ src/main/java/com/devlockin/multiplication/user/domain/User.java
@@ -1,17 +1,22 @@
 package com.devlockin.multiplication.user.domain;
 
 import lombok.*;
-/**
-* Stores information to identify the user.
-*/
-@Getter
-@ToString
-@EqualsAndHashCode
-@AllArgsConstructor
-public class User {
-	
-    private Long id;
-    private String alias;
-    
-}
+import javax.persistence.*;
 
+/**
+ * Stores information to identify the user.
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String alias;
+
+	public User(final String userAlias) {
+		this(null, userAlias);
+	}
+}
