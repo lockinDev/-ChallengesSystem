e11ff293557c945061471f03c49f0cd896f6011e
Javier <javieralmario12@gmail.com>
2021-10-24 23:07:01 -0400

    Added user domain repository

---
+++ src/main/java/com/devlockin/multiplication/user/domain/UserRepository.java
@@ -0,0 +1,8 @@
+package com.devlockin.multiplication.user.domain;
+
+import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+	Optional<User> findByAlias(final String alias);
+}
