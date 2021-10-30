93030ef5dcf9bddf94c3effb8634e584d3223d94
Javier <javieralmario12@gmail.com>
2021-10-29 23:31:02 -0400

    Added ChallengeServiceTest, where is two methods, one for bootstraping every test, and the second test the basic funcionality of the repositories

---
+++ src/test/java/com/devlockin/test/multiplication/challenge/repository/ChallengeServiceTest.java
@@ -0,0 +1,62 @@
+package com.devlockin.test.multiplication.challenge.repository;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.devlockin.multiplication.challenge.domain.ChallengeAttempt;
+import com.devlockin.multiplication.challenge.helper.ChallengeAttemptDTO;
+import com.devlockin.multiplication.challenge.repository.ChallengeAttemptRepository;
+import com.devlockin.multiplication.challenge.service.ChallengeService;
+import com.devlockin.multiplication.challenge.service.ChallengeServiceImpl;
+import com.devlockin.multiplication.user.domain.User;
+import com.devlockin.multiplication.user.domain.UserRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.AdditionalAnswers.returnsFirstArg;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+
+public class ChallengeServiceTest {
+	
+	private ChallengeService challengeService;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private ChallengeAttemptRepository attemptRepository;
+    
+    @BeforeEach
+    public void setUp() {
+        challengeService = new ChallengeServiceImpl(
+                userRepository,
+                attemptRepository
+        );
+        given(attemptRepository.save(any()))
+                .will(returnsFirstArg());
+    }
+    
+    @Test
+    public void checkCorrectAttemptTest() {
+        // given
+        ChallengeAttemptDTO attemptDTO =
+                new ChallengeAttemptDTO(50, 60, "test", 3000);
+        // when
+        ChallengeAttempt resultAttempt =
+                challengeService.verifyAttempt(attemptDTO);
+        // then
+        then(resultAttempt.isCorrect()).isTrue();
+        // newly added lines
+        verify(userRepository).save(new User("test"));
+        verify(attemptRepository).save(resultAttempt);
+    }
+
+}
