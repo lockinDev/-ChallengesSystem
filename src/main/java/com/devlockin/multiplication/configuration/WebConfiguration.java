5dbc4a8a74ca7fce244fcb3df38e43edc3f9bf06
Javier <javieralmario12@gmail.com>
2021-11-10 09:36:06 -0400

    Added new configuration class trying to solve the serialization error loading the jackson’s hibernate module to be picked up by auto-configuration.

--- src/main/java/com/devlockin/multiplication/configuration/WebConfiguration.java
+++ src/main/java/com/devlockin/multiplication/configuration/WebConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
+	
 	@Override
 	public void addCorsMappings(final CorsRegistry registry) {
 		registry.addMapping("/**").allowedOrigins("http://localhost:3000");
