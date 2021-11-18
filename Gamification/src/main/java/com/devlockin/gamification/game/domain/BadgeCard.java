209a45ee6227d8eedacb1ce832728931ea2f08ef
Javier <javieralmario12@gmail.com>
2021-11-18 09:04:17 -0400

    Added the BadgeCard domain/data class

---
+++ Gamification/src/main/java/com/devlockin/gamification/game/domain/BadgeCard.java
@@ -0,0 +1,26 @@
+package com.devlockin.gamification.game.domain;
+
+import lombok.*;
+import javax.persistence.*;
+
+import com.devlockin.gamification.game.helper.BadgeType;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BadgeCard {
+
+	@Id
+	@GeneratedValue
+	private Long badgeId;
+	private Long userId;
+	@EqualsAndHashCode.Exclude
+	private long badgeTimestamp;
+	private BadgeType badgeType;
+
+	public BadgeCard(final Long userId, final BadgeType badgeType) {
+		this(null, userId, System.currentTimeMillis(), badgeType);
+	}
+
+}
