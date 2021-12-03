55249dc4a84bf1827e4011a26315b5531a94458d
Javier <javieralmario12@gmail.com>
2021-12-03 10:03:42 -0400

    Added FirstWonBadgeProcessor implementation.

---
+++ Gamification/src/main/java/com/devlockin/gamification/game/helper/FirstWonBadgeProcessor.java
@@ -0,0 +1,23 @@
+package com.devlockin.gamification.game.helper;
+
+import org.springframework.stereotype.Component;
+
+import com.devlockin.gamification.game.domain.ScoreCard;
+
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class FirstWonBadgeProcessor implements BadgeProcessor {
+	@Override
+	public Optional<BadgeType> processForOptionalBadge(int currentScore, List<ScoreCard> scoreCardList,
+			ChallengeSolvedDTO solved) {
+
+		return scoreCardList.size() == 1 ? Optional.of(BadgeType.FIRST_WON) : Optional.empty();
+	}
+
+	@Override
+	public BadgeType badgeType() {
+		return BadgeType.FIRST_WON;
+	}
+}
