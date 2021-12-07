a2859eab634b78d49df3790f45a4a84f87112d0d
Javier <javieralmario12@gmail.com>
2021-12-07 09:17:04 -0400

    Modified format in those class.

--- Gamification/src/main/java/com/devlockin/gamification/game/repository/BadgeRepository.java
+++ Gamification/src/main/java/com/devlockin/gamification/game/repository/BadgeRepository.java
@@ -1,11 +1,13 @@
 package com.devlockin.gamification.game.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.devlockin.gamification.game.domain.BadgeCard;
 
 import java.util.List;
 
+@Repository
 public interface BadgeRepository extends CrudRepository<BadgeCard, Long> {
 	/**
 	 * Retrieves all BadgeCards for a given user.
--- Gamification/src/main/java/com/devlockin/gamification/game/service/GameService.java
+++ Gamification/src/main/java/com/devlockin/gamification/game/service/GameService.java
@@ -3,6 +3,7 @@ package com.devlockin.gamification.game.service;
 import java.util.List;
 
 import com.devlockin.gamification.game.helper.BadgeType;
+import com.devlockin.gamification.game.helper.ChallengeSolvedDTO;
 
 import lombok.Value;
 
@@ -11,7 +12,7 @@ public interface GameService {
 	 * Process a new attempt from a given user.
 	 ** 
 	 * @param challenge the challenge data with user details, factors, etc.
-	 *                   @return a {@link GameResult} object containing the new
+	 *                  @return a {@link GameResult} object containing the new
 	 *                  score and badge cards obtained
 	 */
 	GameResult newAttemptForUser(ChallengeSolvedDTO challenge);
