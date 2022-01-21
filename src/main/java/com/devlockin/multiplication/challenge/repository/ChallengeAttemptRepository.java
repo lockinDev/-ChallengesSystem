b04396bbe618f376a5567f0d0b7d0523b7025c43
Javier <javieralmario12@gmail.com>
2022-01-20 22:09:17 -0400

    Added repository annotation to repository classes

--- src/main/java/com/devlockin/multiplication/challenge/repository/ChallengeAttemptRepository.java
+++ src/main/java/com/devlockin/multiplication/challenge/repository/ChallengeAttemptRepository.java
@@ -1,11 +1,13 @@
 package com.devlockin.multiplication.challenge.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.devlockin.multiplication.challenge.domain.ChallengeAttempt;
 
 import java.util.List;
 
+@Repository
 public interface ChallengeAttemptRepository extends CrudRepository<ChallengeAttempt, Long> {
     /**
      * @return the last 10 attempts for a given user, identified by their alias.
--- src/main/java/com/devlockin/multiplication/user/repository/UserRepository.java
+++ src/main/java/com/devlockin/multiplication/user/repository/UserRepository.java
@@ -1,12 +1,14 @@
 package com.devlockin.multiplication.user.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.devlockin.multiplication.user.domain.User;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 	Optional<User> findByAlias(final String alias);
 	List<User> findAllByIdIn(final List<Long> ids);
