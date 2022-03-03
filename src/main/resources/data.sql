ac5e34fea02532802e715a81a7dfa27af3722f2a
Javier <javieralmario12@gmail.com>
2022-03-02 22:53:42 -0400

    Added docker-compose file to iniciate rabbitmq. Modified init/boot class in pom file.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 DELETE FROM user;
 INSERT  INTO user (id, alias) VALUES (1, 'bill');
 INSERT  INTO user (id, alias) VALUES (2, 'rachel');
-INSERT  INTO user (id, alias) VALUES (3, 'bill');
-
+INSERT  INTO user (id, alias) VALUES (3, 'bill'); 
+ 
 
